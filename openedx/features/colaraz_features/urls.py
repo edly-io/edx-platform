a6cdd41f18eb4ecc83d2822faab802b7b9259a29
HamzaIbnFarooq <hamza.farooq@arbisoft.com>
2020-04-20 17:00:10 +0500

    changing logout process

--- openedx/features/colaraz_features/urls.py
+++ openedx/features/colaraz_features/urls.py
@@ -4,10 +4,13 @@ URL patterns for colaraz features application.
 
 from django.conf.urls import url, include
 from openedx.features.colaraz_features.api import urls
-
+from openedx.features.colaraz_features.views import AuthProviderLogoutRedirectView
 
 urlpatterns = [
     url(
-        r'^v1/', include(urls.urlpatterns)
+        r'^v1/', include(urls.urlpatterns),
+    ),
+    url(
+        r'^auth_logout_redirect/', AuthProviderLogoutRedirectView.as_view(), name='auth_logout_redirect'
     ),
 ]
