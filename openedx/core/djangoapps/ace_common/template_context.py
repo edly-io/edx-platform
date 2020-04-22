8a19160b4716151d043abf7773e974b1bd352422
Asad Manzoor <asad33manzoor@gmail.com>
2020-04-22 20:57:13 +0500

    Code Clean

--- openedx/core/djangoapps/ace_common/template_context.py
+++ openedx/core/djangoapps/ace_common/template_context.py
@@ -38,7 +38,7 @@ def get_base_template_context(site):
     except NoReverseMatch:
         dashboard_url = reverse('home')
 
-    contact_mailing_address = configuration_helpers.get_value('CONTACT_MAILING_ADDRESS1')
+    contact_mailing_address = configuration_helpers.get_value('CONTACT_MAILING_ADDRESS')
 
     if not contact_mailing_address:
         contact_mailing_address = get_config_value_from_site_or_settings(
