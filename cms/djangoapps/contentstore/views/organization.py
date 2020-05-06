b484ee205ab8beaffe469f1fb76cc610ddadff5a
Burhan Nasir <burhandodhy@gmail.com>
2020-05-06 17:08:48 +0500

    show only organization that is link with site

--- cms/djangoapps/contentstore/views/organization.py
+++ cms/djangoapps/contentstore/views/organization.py
@@ -3,9 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.generic import View
+from django.contrib.sites.shortcuts import get_current_site
+
+from openedx.features.edly.models import EdlySubOrganization
 
 from openedx.core.djangolib.js_utils import dump_js_escaped_json
-from util.organizations_helpers import get_organizations
 
 
 class OrganizationListView(View):
@@ -18,6 +20,10 @@ class OrganizationListView(View):
     @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
         """Returns organization list as json."""
-        organizations = get_organizations()
-        org_names_list = [(org["short_name"]) for org in organizations]
+
+        current_site = get_current_site(request)
+
+        edly_sub_organization = EdlySubOrganization.objects.filter(studio_site=current_site.id)
+
+        org_names_list = [(org.edx_organization.short_name ) for org in edly_sub_organization]
         return HttpResponse(dump_js_escaped_json(org_names_list), content_type='application/json; charset=utf-8')
--- cms/djangoapps/contentstore/views/tests/test_organizations.py
+++ cms/djangoapps/contentstore/views/tests/test_organizations.py
@@ -7,7 +7,7 @@ from mock import patch
 
 from student.tests.factories import UserFactory
 from util.organizations_helpers import add_organization
-
+from openedx.features.edly.tests.factories import EdlyOrganizationFactory, EdlySubOrganizationFactory, SiteFactory
 
 @patch.dict('django.conf.settings.FEATURES', {'ORGANIZATIONS_APP': True})
 class TestOrganizationListing(TestCase):
@@ -18,17 +18,26 @@ class TestOrganizationListing(TestCase):
         self.staff = UserFactory(is_staff=True)
         self.client.login(username=self.staff.username, password='test')
         self.org_names_listing_url = reverse('organizations')
-        self.org_short_names = ["alphaX", "betaX", "orgX"]
-        for index, short_name in enumerate(self.org_short_names):
-            add_organization(organization_data={
-                'name': 'Test Organization %s' % index,
-                'short_name': short_name,
-                'description': 'Testing Organization %s Description' % index,
-            })
 
     def test_organization_list(self):
-        """Verify that the organization names list api returns list of organization short names."""
-        response = self.client.get(self.org_names_listing_url, HTTP_ACCEPT='application/json')
+        """
+        Verify that the organization names list api returns list of organization short names.
+        """
+
+        studio_site = SiteFactory()
+        edly_organization = EdlyOrganizationFactory(name='Test Edly Organization Name')
+        edly_sub_organization = EdlySubOrganizationFactory(
+            name='Test Edly Sub Organization Name',
+            slug='test-edly-sub-organization-name',
+            studio_site=studio_site,
+            edly_organization=edly_organization
+        )
+
+        organization = edly_sub_organization.edx_organization
+        organization.short_name  = 'test-edx-organization'
+        organization.save();
+
+        response = self.client.get(self.org_names_listing_url, HTTP_ACCEPT='application/json', SERVER_NAME=studio_site.domain)
+
         self.assertEqual(response.status_code, 200)
-        org_names = json.loads(response.content)
-        self.assertEqual(org_names, self.org_short_names)
+        self.assertEqual(response.json()[0], edly_sub_organization.edx_organization.short_name)
