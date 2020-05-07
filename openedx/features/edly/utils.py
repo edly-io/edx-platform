5051506191f38440183fb9f4beb741a6eadeb1a8
Burhan Nasir <burhandodhy@gmail.com>
2020-05-07 16:06:44 +0500

    Feedback implement

--- openedx/features/edly/utils.py
+++ openedx/features/edly/utils.py
@@ -1,11 +1,22 @@
+import waffle
+from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from openedx.features.edly.models import EdlySubOrganization
+from util.organizations_helpers import get_organizations
 
+def get_enabled_organization(request):
+  """
+  Helper method to get linked organizations for request site.
 
-def get_edly_organizations(request):
+  Returns:
+      list: List of linked organizations for request site
   """
-  Returns Organization base on request site.
-  """
-  current_site = get_current_site(request)
-  edly_organizations = EdlySubOrganization.objects.filter(studio_site=current_site.id)
-  return edly_organizations
+
+  if not waffle.switch_is_active(settings.ENABLE_EDLY_ORGANIZATIONS_SWITCH):
+      organizations = get_organizations()
+  else:
+      current_site = get_current_site(request)
+      edly_organizations = EdlySubOrganization.objects.filter(studio_site=current_site.id)
+      organizations = [(org.edx_organization.__dict__) for org in edly_organizations]
+
+  return organizations
