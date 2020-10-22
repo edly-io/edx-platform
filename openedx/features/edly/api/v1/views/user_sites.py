a6de4bb579b124ddcebf888b77cdd526be9b8156
Haseeb <muhammad.haseeb@edly.io>
2020-10-22 16:34:41 +0500

    Added docstring

--- openedx/features/edly/api/v1/views/user_sites.py
+++ openedx/features/edly/api/v1/views/user_sites.py
@@ -1,3 +1,6 @@
+"""
+Views for user sites API
+"""
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -8,6 +11,33 @@ from openedx.features.edly.api.serializers import UserSiteSerializer
 
 @mobile_view()
 class UserSitesViewSet(viewsets.ViewSet):
+    """
+    **Use Case**
+
+        Get information about the current user's sites and mobile configurations
+        of the sites with which user is linked.
+
+        Apps hit this endpoint after obtaining access token for a user.
+
+        You can use the **app_configs** value in the response to get a
+        mobile configurations for the sites with user is linked.
+
+        You can use the **site_data** value in the response to get a
+        branding configurations for the sites with user is linked.
+
+    **Example Request**
+
+        GET /api/mobile/v1/user-sites/
+
+    **Response Values**
+
+        If the request is successful, the request returns an HTTP 200 "OK" response.
+
+        The HTTP 200 response has the following values.
+
+        * app_config: Mobile configurations for the site with user is linked.
+        * site_data: Branding configurations for the site with user is linked.
+    """
     permission_classes = (IsAuthenticated,)
     serializer = UserSiteSerializer
 
