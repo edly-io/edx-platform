e178fe096d08e486c8ccdf32d42737c03525f2e1
Asad Manzoor <asad33manzoor@gmail.com>
2021-11-04 17:44:35 +0500

    Update Course Rating API endpoint to list all the ratings

--- openedx/features/course_rating/api/v1/urls.py
+++ openedx/features/course_rating/api/v1/urls.py
@@ -9,7 +9,7 @@ from django.conf import settings
 from openedx.features.course_rating.api.v1.views import CourseRatingViewSet, CourseAverageRatingAPIView
 
 router = routers.DefaultRouter()
-router.register(r'course_rating/{}'.format(settings.COURSE_ID_PATTERN), CourseRatingViewSet, base_name='course_rating')
+router.register(r'course_rating', CourseRatingViewSet, base_name='course_rating')
 
 app_name = 'v1'
 
