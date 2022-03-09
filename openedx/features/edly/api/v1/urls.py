d156863ab181f14b9fa3e10deadf1281a0febefb
Hamza Madni <hamza.madni@arbisoft.com>
2022-03-09 16:45:02 +0500

    Adding course enrollment api endpoint in edly app.

--- openedx/features/edly/api/v1/urls.py
+++ openedx/features/edly/api/v1/urls.py
@@ -2,7 +2,14 @@ from rest_framework import routers
 
 from openedx.features.edly.api.v1.views.user_sites import UserSitesViewSet
 
+from figures.views import CourseEnrollmentViewSet
+
 router = routers.SimpleRouter()
 router.register(r'user_sites', UserSitesViewSet, base_name='user_sites')
+router.register(
+    r'course_enrollment',
+    CourseEnrollmentViewSet,
+    base_name='course_enrollments'
+    )
 
 urlpatterns = router.urls
