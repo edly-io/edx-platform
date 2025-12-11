e0357a7857e28bb291cf4429e1fbed74b1a29913
Muhammad Faraz  Maqsood <faraz.maqsood@A006-01130.local>
2025-12-11 11:38:17 +0500

    test: okta changes over release teak.2 tag

---
+++ common/djangoapps/third_party_auth/okta.py
@@ -0,0 +1,34 @@
+# pylint: disable=W0223
+"""
+python-social-auth backend for use with Okta
+link: https://github.com/python-social-auth/social-core/blob/master/social_core/backends/okta_openidconnect.py
+"""
+from logging import getLogger
+
+from social_core.backends.okta_openidconnect import OktaOpenIdConnect
+from django.utils.functional import cached_property
+
+logger = getLogger(__name__)
+
+
+class OktaOAuth2(OktaOpenIdConnect):
+    """
+    An extension of the OktaOpenIdConnect for use with an OktaOAuth2 service.
+    link: https://github.com/python-social-auth/social-core/blob/master/social_core/backends/okta_openidconnect.py
+    """
+
+    def get_user_details(self, response):
+        """
+        Return details about the user account from the service
+        """
+        user_details = {
+            "username": response.get("preferred_username"),
+            "email": response.get("email") or "",
+            "first_name": response.get("given_name"),
+            "last_name": response.get("family_name"),
+            "roles": response.get("roles"),
+            "city": response.get("city"),
+        }
+        logger.info("OktaOAuth2 raw response: %s", response)
+        logger.info("OktaOAuth2 user_details: %s", user_details)
+        return user_details
