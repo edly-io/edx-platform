ed34246a4d8518acb92ddcb715efaf8e68379d32
Taimoor  Ahmed <taimoor.ahmed@A006-01711.local>
2026-04-29 12:23:21 +0500

    feat: apply ADR 0028 to HomePageCoursesView

--- cms/djangoapps/contentstore/rest_api/v1/views/home.py
+++ cms/djangoapps/contentstore/rest_api/v1/views/home.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 from edx_rest_framework_extensions.auth.session.authentication import SessionAuthenticationAllowInactiveUser
 from organizations import api as org_api
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
